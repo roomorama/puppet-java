e17346776d0cd06c0447b5276d9d337eb2ad373b
Donald Piret <donald@donaldpiret.com>
2013-11-04 14:58:42 +0800

    -C option needs an offset

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,12 +20,12 @@ class java {
   $wrapper = "${boxen::config::bindir}/java"
 
   exec { 'download-jre':
-    command   => "/usr/bin/curl -o ${jre_dmg_location} -C -k -L -s --header 'Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F;' http://download.oracle.com/otn-pub/java/jdk/${jdk_version}-b${jdk_build_number}/jre-${jre_version}-macosx-x64.dmg",
+    command   => "/usr/bin/curl -o ${jre_dmg_location} -k -L -s --header 'Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F;' http://download.oracle.com/otn-pub/java/jdk/${jdk_version}-b${jdk_build_number}/jre-${jre_version}-macosx-x64.dmg",
     creates   => $jre_dmg_location,
   }
 
   exec { 'download-jdk':
-    command   => "/usr/bin/curl -o ${jdk_dmg_location} -C -k -L -s --header 'Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F;' http://download.oracle.com/otn-pub/java/jdk/${jdk_version}-b${jdk_build_number}/jdk-${jdk_version}-macosx-x64.dmg",
+    command   => "/usr/bin/curl -o ${jdk_dmg_location} -k -L -s --header 'Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F;' http://download.oracle.com/otn-pub/java/jdk/${jdk_version}-b${jdk_build_number}/jdk-${jdk_version}-macosx-x64.dmg",
     creates   => $jdk_dmg_location,
   }
 
